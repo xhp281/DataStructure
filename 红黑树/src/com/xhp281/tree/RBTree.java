86566067b49c8121a72e95b045a9c1501da22409
xhp281 <xhp281@gmail.com>
2019-05-30 18:57:51 +0800

    代码优化

--- 红黑树/src/com/xhp281/tree/RBTree.java
+++ 红黑树/src/com/xhp281/tree/RBTree.java
@@ -42,7 +42,7 @@ public class RBTree<E> extends BBST<E> {
         // 叔父节点
         Node<E> uncle = parent.sibling();
         // 祖父节点
-        Node<E> grand = parent.parent;
+        Node<E> grand = red(parent.parent);
 
         // 叔父节点是红色
         // parent染成黑色，grand染成红色
@@ -50,13 +50,12 @@ public class RBTree<E> extends BBST<E> {
             black(parent);
             black(uncle);
             // 把祖父节点当做是新添加的节点,进行递归调用，更新祖父节点之上的节点
-            addAfterFixNode(red(grand));
+            addAfterFixNode(grand);
             return;
         }
 
         // 叔父节点不是红色
         if (parent.isLeftChild()){   // L
-            red(grand);
             if (node.isLeftChild()){
                 // LL parent 染成 BLACK，grand 染成 RED
                 black(parent);
@@ -68,7 +67,6 @@ public class RBTree<E> extends BBST<E> {
             rotateRight(grand);
         }else{ // R
 
-            red(grand);
             if (node.isLeftChild()){
                 // RL 自己染成 BLACK，grand 染成 RED；parent 右旋转， grand 左旋转
                 black(node);
