76c6c095bd2c3a58d85ba14fbe3569ce6d1dd1bd
xhp281 <xhp281@gmail.com>
2019-06-04 18:28:06 +0800

    - 调整添加删除

--- 映射/src/com/xhp281/map/TreeMap.java
+++ 映射/src/com/xhp281/map/TreeMap.java
@@ -50,8 +50,7 @@ public class TreeMap<K,V> implements Map<K, V> {
             root = new Node<>(key,value,null);
             size++;
             // 新添加节点之后的处理
-//            afterPut(root);
-            black(root);
+            afterPut(root);
             return null;
         }
 
@@ -177,6 +176,9 @@ public class TreeMap<K,V> implements Map<K, V> {
             afterRemove(replacement);
         } else if (node.parent == null) { // node是叶子节点并且是根节点
             root = null;
+
+            // 删除节点之后的处理
+            afterRemove(node);
         } else { // node是叶子节点，但不是根节点
             if (node == node.parent.left) {
                 node.parent.left = null;
@@ -241,6 +243,8 @@ public class TreeMap<K,V> implements Map<K, V> {
         }
 
         Node<K,V> parent = node.parent;
+        // 删除的是根节点
+        if (parent == null) return;
 
         // 删除的是黑色叶子节点【下溢】
         // 判断被删除的node是左还是右
